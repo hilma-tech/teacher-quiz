449a416c90a871ea358aa3709d3f336f5123e046
physics <quantumphysics100@gmail.com>
2020-04-30 19:12:35 +0300

    styles and scss

--- src/Answers.jsx
+++ src/Answers.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AlexaRecorder from './Recorder';
-import './answers.css';
+import './styles/scss/answers.scss';
+import './styles/css/answers.css';
 
 class AnswersAndRecords extends Component{
     constructor(props){
@@ -18,9 +19,10 @@ class AnswersAndRecords extends Component{
 
     render(){
         return(
-            <div className="main">
+            <div className="main-answers">
                 <AlexaRecorder/>
                 <input 
+                pattern="[A-Za-z]" //doen't work :/
                 type="text"
                 placeholder="type here your answer"
                 onChange={this.handleAnswer}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import './home.css';
+import './styles/scss/home.scss';
+// import './styles/css/home.css';
 import { Link } from 'react-router-dom';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import DeleteIcon from '@material-ui/icons/Delete';
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+import EditIcon from '@material-ui/icons/Edit';
 
 
 
@@ -12,13 +15,12 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            challenges: [{ name: 'places around the world' },
-            { name: 'food names' }]
+            challenges: [{ name: 'places around the world', inEditMode: false },
+            { name: 'food names', inEditMode: false }]
         }
     }
 
     deleteChallenge = (event) => {
-        console.log('event: ', event.target, 'current', event.currentTarget);
         // window.alert("are you sure that you want to delete this challenge?")
         let challenges = this.state.challenges
         let index = event.currentTarget.getAttribute("index");
@@ -35,28 +37,77 @@ class Home extends Component {
         this.setState({ challenges });
     }
 
+    addNewChallenge = ()=>{
+        let challenges = this.state.challenges
+        challenges.push({inEditMode: true, name: ''})
+        this.setState({challenges})
+    }
+
+    edit = (event)=>{
+        let challenges = this.state.challenges
+        let index=event.currentTarget.getAttribute("index")
+        challenges[index].inEditMode = true;
+        this.setState({challenges})
+    }
+
+    editOnEnter = (event)=>{
+        let challenges = this.state.challenges
+        let index=event.currentTarget.getAttribute("index")
+        if(event.key==="Enter"){
+            challenges[index].inEditMode=false;
+            this.setState({challenges})
+        }
+    }
+
+    handleAnswer = (event)=>{
+        let challenges = this.state.challenges
+        let value = event.target.value
+        let index = event.target.getAttribute("index")
+        challenges[index].name = value
+        this.setState({challenges})
+    }
 
     render() {
         return (
-            <div>
-                <h1>שלום מורה!</h1>
-                <h3>האתגרים שיצרת</h3>
+            <div className="home">
+                <div className="main">
+                <h1 className="title">שלום מורה!</h1>
+                <h3 className="title">האתגרים שיצרת</h3>
                 <div>
                     {this.state.challenges.map((chall, index) => {
                         return (
-                            <div>
-                                <Link to='/questions'>
-                                    <div className="challenges">
+                            <div className="challenge-list">
+                                {!chall.inEditMode &&
+                            <div className="challenges">
+                                <Link to='/questions' className="link">
+                                    <div className="name">
                                         {chall.name}
                                     </div>
                                 </Link>
-                                {/* <button index={index} onClick={(event) => this.deleteChallenge(event)}> */}
                                     <DeleteIcon index={index} onClick={this.deleteChallenge}/>
-                                {/* </button> */}
+                                    <EditIcon index={index} onClick={this.edit}/>
+                            </div>}
+                                {chall.inEditMode &&
+                                <div className="challenges">
+                                    <input 
+                                    index={index}
+                                    className="input"
+                                    type="text" 
+                                    placeholder="name here"
+                                    value={chall.name}
+                                    onChange={this.handleAnswer}
+                                    onKeyPress={this.editOnEnter}
+                                    ></input>
+                                    <DeleteIcon index={index} onClick={this.deleteChallenge}/>
+                                    </div>}
                             </div>
-
                         )
                     })}
+                    <ControlPointIcon 
+                    className="button" 
+                    onClick={this.addNewChallenge}
+                    fontSize="large"
+                    />
                 </div>
                 {/* <div>
                     <InputLabel htmlFor="input-with-icon-adornment">With a start adornment</InputLabel>
@@ -71,6 +122,7 @@ class Home extends Component {
                         }
                     />
                 </div> */}
+            </div>
             </div>
         )
     }
